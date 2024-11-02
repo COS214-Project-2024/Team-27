99768d9f13eb78691c24456f2bcdf08d0c831510
Jay-9-9-9 <u22625926@tuks.co.za>
2024-11-02 19:23:03 +0200

    changing the Memento Constructor

--- GovernmentMemento.cpp
+++ GovernmentMemento.cpp
@@ -1,7 +1,7 @@
 #include "GovernmentMemento.h"
 
-GovernmentMemento::GovernmentMemento(double b, double c, vector<Building*> buildingsC, vector<Citizen*> CitizensC)
-    : buildingTaxRate(b), citizenTaxRate(c), savedPopulation(population), savedNumberOfBuildings(numBuildings), savedEconomicGrowth(econRate),buildings(buildingsC), citizens(CitizensC){}
+GovernmentMemento:: GovernmentMemento(double b, double c,int population, int numBuildings, double econRate, vector<Building*> buildingsC, vector<Citizen*> CitizenC)
+    : buildingTaxRate(b), citizenTaxRate(c), savedPopulation(population),  savedNumberOfBuildings(numBuildings), savedEconomicGrowthRate(econRate),buildings(buildingsC), citizens(CitizenC){}
 
 
 void GovernmentMemento::getDetails(){
