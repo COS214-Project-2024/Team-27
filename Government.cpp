5ee9725870b2a304a78a067fe6e22773e41f101c
Lineo-dev01 <u23604043@tuks.co.za>
2024-11-03 16:01:42 +0200

    Update Government.cpp

--- Government.cpp
+++ Government.cpp
@@ -55,3 +55,31 @@ void Government::displayState(){
 void Government::addBuilding(Building* b){
     buildings.push_back(b);
 }
+
+void Government::collectCitizenTax() {
+
+   int i=1;
+   int j=1;
+
+ for (Citizen citizen : citizens) {
+  
+   if(citizen.getEmploymentStatus() && citizen.getincome()>=3000){
+    double tax= citizen.getincome() * citizenTaxRate;
+    CTotalTax+=tax;
+          std::cout <<"( "<< i++<< ") Collected " << tax << " from citizen." << std::endl;
+   }
+   else{
+    ///cout user does not qualiy to pay tax
+   }
+       
+ }
+ }
+void Government::collectBuildingTax(){
+
+}
+ void Government::collectCityTax(){
+    
+ collectCitizenTax();
+    collectBuildingTax();
+    std::cout << "City tax collection complete." << std::endl;
+ }
