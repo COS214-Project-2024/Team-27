832d5460371d112f82670c9ca0d6dcdf1023c5b7
RethaMokoena <162606472+RethaMokoena@users.noreply.github.com>
2024-11-03 14:24:04 +0200

    PowerSysterm changed

--- PowerSystem.cpp
+++ PowerSystem.cpp
@@ -4,10 +4,11 @@ PowerSystem::PowerSystem(double megawatts)
 {
     this->megawatts = megawatts;
     this->save = megawatts;
+    this->f = true;
     
 }
 
-void PowerSystem::reboot()
+void PowerSystem::update()
 {
     megawatts+=save;
 }
@@ -16,6 +17,7 @@ void PowerSystem::useResources(double used)
 {
     if (used > megawatts)
     {
+        f = false;
         std::cout<<"Not enough power "<<std::endl;
     }else{
         megawatts-= used;
